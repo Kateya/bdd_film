0e27a0c7873a672fe4adfeb6528531b69bd4a293
Kateya <dubarry.cynthia@gmail.com>
2017-11-30 16:19:18 +0100

    add htaccess file

--- views/filmsView.php
+++ views/filmsView.php
@@ -8,8 +8,7 @@
 		<?php foreach ($films as $key => $film): ?>
 			
 			<div class="film>">
-				<h3><?=$film['Titre']?></h3>
-
+			<a href="film/<?= $key+1 ?>"><h3><?=$film['Titre']?></h3></a>
 			</div>
 
 		<?php endforeach ?>
